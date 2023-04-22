e0d463023c719ff1f1d2ce4012506aae19f8fe27
Arkadiusz-Kapa <arkadiusz_kapa.outlook.com>
2023-04-22 10:43:26 +0200

    poczátek tworzenia bohatera

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,1 +1,15 @@
 import keyboard, os, random, time
+
+#Zmienne globalne:
+width, height = 200, 20
+Bird_OY_Position = height // 2
+
+
+#Budowa Ptaka/ gracza:
+
+class Bird():
+    
+    def D_Bird(self, x ,y):
+        Bird_OY_Position = y
+        face = '>'
+        body = '[]'
