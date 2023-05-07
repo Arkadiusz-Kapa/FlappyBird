683444f74b2de921db4b0cc42294baa2cfecbd8d
Arkadiusz-Kapa <arkadiusz_kapa.outlook.com>
2023-05-07 13:10:20 +0200

    poczatek klasy game oraz pisanei funckji tworzacej rury

--- FlappyBird.py
+++ FlappyBird.py
@@ -1,8 +1,16 @@
 import keyboard, os, random, time
 
 #Zmienne globalne:
-width, height = 200, 20
+width, height = 200, 20 #ustawienie wysokości i szerokości
 Bird_OY_Position = height // 2
+pipeSplitsQueue = []
+gameBoard = [[0 for x in range(w)] for y in range(h)]
+collision = False
+
+class Game():
+    def Pipe(self,OX_position, spli): #Funkcja rysująca rure
+        for i in range(height): # petla w zakresie do zmiennej height (wazne aby nei bylo tam stalej liczby a zmienna jesli chcemy latwiej zmieniac wielkosc ekranu gry)
+            if(not(split-3 <= 1 <= split))
 
 
 #Budowa Ptaka/ gracza:
@@ -10,6 +18,6 @@ Bird_OY_Position = height // 2
 class Bird():
     
     def D_Bird(self, x ,y):
-        Bird_OY_Position = y
+        global Bird_OY_Position = y
         face = '>'
         body = '[]'
