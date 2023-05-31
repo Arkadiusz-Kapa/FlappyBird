c58439fce48a437adca6e765ba10baec511c7ed4
Arkadiusz-Kapa <arkadiusz_kapa@outlook.com>
2023-05-31 16:32:35 +0200

    .

--- menu.py
+++ menu.py
@@ -1,17 +1,5 @@
 import keyboard, subprocess, os
 
-width, height = 100, 30
-menu = [[0 for x in range(width)] for y in range(height)]
-level = 0
-class menu():
-    for i in range(height): 
-        for j in range(width):
-            menu[i][j] ='-' if (i < 8) else '='
-    for i in range(height):
-        res = " "
-        for j in range(width):
-            res += menu[i][j]    
-        print(res)
     os.system('cls')
     print("navigate by pressing W or S")
     if keyboard.is_pressed("w"): level += 1
